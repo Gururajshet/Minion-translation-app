8ac736d088f4c42bc137639011da49ed061fe3d0
Gururajshet <86518851+Gururajshet@users.noreply.github.com>
2021-08-16 20:33:28 +0530

    integrated an  API

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 'use strict'
 
+const textArea = document.getElementById('text__area');
+const buttonClick = document.getElementById('btn');
+const translatedArea = document.getElementById('block');
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+
+
+
+
+function updatedUrl(url) {
+    return `${url}?text=${textArea.value}`
+}
+
+function translated() {
+    fetch(updatedUrl(url))
+        .then(Response => Response.json()
+            .then(json => translatedArea.innerText =json.contents.translated))
+}
+
+buttonClick.addEventListener('click',translated)
